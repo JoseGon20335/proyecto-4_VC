6b88781d693e36052b8f5a44a9ad93d58f46da88
Stefano <69864405+stefanoaragoni@users.noreply.github.com>
2024-05-24 09:17:10 -0600

    Update buildDataSet.py

--- buildDataSet.py
+++ buildDataSet.py
@@ -1,5 +1,20 @@
-# Proyecto # 4 - Vision por computadora
-# @ buildDataSet.py - encargado de construir el .pickle con los datos.
+# ----------------------------------------------------------------------
+
+# Visión por Computadora
+# Proyecto 4: Detección de Alfabeto de LENSEGUA
+
+# Archivo: buildDataSet.py
+# Descripción: Este programa se encarga de procesar las imágenes capturadas con addData.py para extraer los puntos de referencia de las manos detectadas.
+#              Los puntos de referencia se normalizan y se guardan en un archivo pickle para su uso en el entrenamiento del modelo.
+
+# Autores:
+# Stefano Alberto Aragoni Maldonado - 20261
+# Carol Andreé Arévalo Estrada - 20461
+# José Miguel González González - 20335
+# Luis Diego Santos Cuellar - 20226
+
+# ----------------------------------------------------------------------
+# IMPORTAR LIBRERÍAS
 
 import os
 import pickle
@@ -7,53 +22,63 @@ import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
-# Inicialización de mediapipe para detección de manos
-mpHands = mp.solutions.hands
-mpDrawing = mp.solutions.drawing_utils
-mpDrawingStyles = mp.solutions.drawing_styles
+# ----------------------------------------------------------------------
+# VARIABLES GLOBALES
 
-# Configuración del módulo de manos de mediapipe
-handsDetector = mpHands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+mpHands = mp.solutions.hands                                            # Módulo de manos de mediapipe
+mpDrawing = mp.solutions.drawing_utils                                  # Módulo de dibujo de mediapipe
+mpDrawingStyles = mp.solutions.drawing_styles                           # Estilos de dibujo de mediapipe
 
-# Directorio de datos
-dataDirectory = './imgs'
+handsDetector = mpHands.Hands(static_image_mode=True, min_detection_confidence=0.3)     # Inicialización de detector de manos
 
-handLandmarksData = []  # Lista para almacenar datos procesados
-classLabels = []  # Lista para almacenar etiquetas de clases
+dataDirectory = './imgs'                                                # Directorio de imágenes a procesar
 
-# Recorre los directorios dentro de dataDirectory (cada uno representa una clase)
-for classDir in os.listdir(dataDirectory):
-    # Recorre las imágenes dentro del directorio de cada clase
-    for imageFilename in os.listdir(os.path.join(dataDirectory, classDir)):
-        landmarksNormalized = []  # Lista auxiliar para almacenar coordenadas normalizadas
-        xCoordinates = []  # Lista para almacenar coordenadas x de los puntos de referencia
-        yCoordinates = []  # Lista para almacenar coordenadas y de los puntos de referencia
+handLandmarksData = []                                                  # Lista para almacenar datos de puntos de referencia
+classLabels = []                                                        # Lista para almacenar etiquetas de las clases
 
-        # Lee la imagen y la convierte a RGB
-        imagePath = os.path.join(dataDirectory, classDir, imageFilename)
-        image = cv2.imread(imagePath)
-        if image is None:
+# ----------------------------------------------------------------------
+
+
+for classDir in os.listdir(dataDirectory):                                      # Recorre las clases dentro del directorio de imágenes
+    for imageFilename in os.listdir(os.path.join(dataDirectory, classDir)):     # Recorre las imágenes dentro de cada clase
+
+        landmarksNormalized = []                                                # Lista para almacenar puntos de referencia normalizados
+        xCoordinates = []                                                       # Lista para almacenar coordenadas x de los puntos de referencia           
+        yCoordinates = []                                                       # Lista para almacenar coordenadas y de los puntos de referencia
+
+        imagePath = os.path.join(dataDirectory, classDir, imageFilename)        # Ruta de la imagen a procesar
+        image = cv2.imread(imagePath)                                           # Lee la imagen            
+        if image is None:                                                       # Verifica si la imagen se pudo leer correctamente
             print(f"Error: No se pudo leer la imagen {imagePath}")
             continue
-        imageRgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Procesa la imagen para detectar manos
-        results = handsDetector.process(imageRgb)
-        if results.multi_hand_landmarks:
-            # Si se detectan manos, se procesan los puntos de referencia
-            for handLandmarks in results.multi_hand_landmarks:
-                for landmark in handLandmarks.landmark:
-                    xCoordinates.append(landmark.x)
-                    yCoordinates.append(landmark.y)
 
-                # Normaliza las coordenadas de los puntos de referencia
-                for landmark in handLandmarks.landmark:
-                    landmarksNormalized.append(landmark.x - min(xCoordinates))
-                    landmarksNormalized.append(landmark.y - min(yCoordinates))
+        imageRgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)                       # Convierte la imagen a RGB       
+        r, g, b = cv2.split(imageRgb)                                           # Separa los canales de la imagen
 
-            handLandmarksData.append(landmarksNormalized)  # Añade los datos procesados a la lista principal
-            classLabels.append(classDir)  # Añade la etiqueta de la clase correspondiente
+        r_eq = cv2.equalizeHist(r)                                              # Aplica histogram equalization a cada canal       
+        g_eq = cv2.equalizeHist(g)
+        b_eq = cv2.equalizeHist(b)
 
-# Guarda los datos y etiquetas en un archivo pickle
-with open('data.pickle', 'wb') as pickleFile:
-    pickle.dump({'data': handLandmarksData, 'labels': classLabels}, pickleFile)
+        imageRgb = cv2.merge((r_eq, g_eq, b_eq))                                # Une los canales ecualizados
+
+
+        results = handsDetector.process(imageRgb)                               # Procesa la imagen con el detector de manos
+        if results.multi_hand_landmarks:                                        # Verifica si se detectaron manos en la imagen
+
+            for handLandmarks in results.multi_hand_landmarks:                  # Recorre los puntos de referencia de cada mano detectada
+                for landmark in handLandmarks.landmark:                         # Recorre los puntos de referencia de la mano
+                    xCoordinates.append(landmark.x)                             # Añade la coordenada x del punto de referencia
+                    yCoordinates.append(landmark.y)                             # Añade la coordenada y del punto de referencia
+
+                for landmark in handLandmarks.landmark:                         # Recorre los puntos de referencia de la mano
+                    landmarksNormalized.append(landmark.x - min(xCoordinates))  # Normaliza la coordenada x del punto de referencia
+                    landmarksNormalized.append(landmark.y - min(yCoordinates))  # Normaliza la coordenada y del punto de referencia
+
+            handLandmarksData.append(landmarksNormalized)                       # Añade los puntos de referencia normalizados a la lista de datos
+            classLabels.append(classDir)                                        # Añade la etiqueta de la clase correspondiente
+
+# ----------------------------------------------------------------------
+
+with open('data.pickle', 'wb') as pickleFile:                                   # Guarda los datos de puntos de referencia y las etiquetas en un archivo pickle
+    pickle.dump({'data': handLandmarksData, 'labels': classLabels}, pickleFile) 
