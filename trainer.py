b68b3bda204314701d8b9ab67390391149948673
Stefano <69864405+stefanoaragoni@users.noreply.github.com>
2024-05-24 09:48:21 -0600

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,3 +1,21 @@
+# ----------------------------------------------------------------------
+
+# Visión por Computadora
+# Proyecto 4: Detección de Alfabeto de LENSEGUA
+
+# Archivo: trainer.py
+# Descripción: Este programa se encarga de entrenar un modelo de red neuronal con los datos procesados en buildDataSet.py.
+#              El modelo se guarda en un archivo H5 para su uso en la detección de letras en tiempo real.
+
+# Autores:
+# Stefano Alberto Aragoni Maldonado - 20261
+# Carol Andreé Arévalo Estrada - 20461
+# José Miguel González González - 20335
+# Luis Diego Santos Cuellar - 20226
+
+# ----------------------------------------------------------------------
+# IMPORTAR LIBRERÍAS
+
 import pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -6,41 +24,41 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
 
-# Carga los datos procesados desde un archivo pickle
-dataDict = pickle.load(open('./data.pickle', 'rb'))
+# ----------------------------------------------------------------------
+# VARIABLES GLOBALES
 
-# Verifica la longitud máxima de las secuencias en los datos
-max_length = max(len(seq) for seq in dataDict['data'])
+dataDict = pickle.load(open('./data.pickle', 'rb'))                                                 # Carga los datos procesados en buildDataSet.py
 
-# Rellena las secuencias más cortas con ceros (o un valor de relleno adecuado)
-data = np.array([np.pad(seq, (0, max_length - len(seq)), 'constant') for seq in dataDict['data']])
-labels = np.asarray(dataDict['labels'])
+max_length = max(len(seq) for seq in dataDict['data'])                                              # Se define la longitud máxima de las secuencias
 
-# Normaliza los datos
-data = data / np.max(data)
+data = np.array([np.pad(seq, (0, max_length - len(seq)), 'constant') for seq in dataDict['data']])  # Se rellenan las secuencias con ceros
+data = data / np.max(data)                                                                          # Se normalizan los datos
 
-# Convierte las etiquetas a one-hot encoding
-num_classes = len(np.unique(labels))
-labels = to_categorical(labels, num_classes)
+labels = np.asarray(dataDict['labels'])                                                             # Se obtienen las etiquetas de las secuencias
 
-# Divide los datos en conjuntos de entrenamiento y prueba, manteniendo la proporción de clases
-xTrain, xTest, yTrain, yTest = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+num_classes = len(np.unique(labels))                                                                # Se obtiene el número de clases
+labels = to_categorical(labels, num_classes)                                                        # Se convierten las etiquetas a un formato categórico
 
-# Define la arquitectura de la red neuronal
-model = Sequential()
-model.add(Dense(128, input_dim=xTrain.shape[1], activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+# ----------------------------------------------------------------------
+# ENTRENAMIENTO DE LA RED NEURONAL
 
-# Compila el modelo
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+xTrain, xTest, yTrain, yTest = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels) # Se dividen los datos en entrenamiento y prueba
 
-# Entrena el modelo con los datos de entrenamiento
-model.fit(xTrain, yTrain, epochs=20, batch_size=32, validation_split=0.2)
 
-# Evalúa el modelo con los datos de prueba
-score = model.evaluate(xTest, yTest, verbose=0)
-print('{}% de la data ha sido procesada con éxito.'.format(score[1] * 100))
+model = Sequential()                                                        # Se crea un modelo de red neuronal secuencial / feedforward
+model.add(Dense(128, input_dim=xTrain.shape[1], activation='relu'))         # Capa densa de 128 neuronas con función de activación ReLU
+model.add(Dense(64, activation='relu'))                                     # Capa densa de 64 neuronas con función de activación ReLU
+model.add(Dense(num_classes, activation='softmax'))                         # Capa densa de salida con función de activación Softmax (clasificación multiclase)
 
-# Guarda el modelo entrenado en un archivo HDF5
-model.save('model.h5')
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])  # Se compila el modelo con función de pérdida para clasificación multiclase
+
+model.fit(xTrain, yTrain, epochs=20, batch_size=32, validation_split=0.2)               # Se entrena el modelo con los datos de entrenamiento
+
+score = model.evaluate(xTest, yTest, verbose=0)                                         # Se evalúa el modelo con los datos de prueba
+print('Test loss:', score[0])                                                           # Se imprime la pérdida
+print('Test accuracy:', score[1])                                                       # Se imprime la precisión
+
+# ----------------------------------------------------------------------
+# GUARDAR EL MODELO
+
+model.save('model.h5')                                                                  # Se guarda el modelo en un archivo H5
